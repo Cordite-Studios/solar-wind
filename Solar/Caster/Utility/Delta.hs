ea1f80ede3f6b683085186b4ded3478b30648c09
Levi Schuck <levi.schuck@gmail.com>
2013-10-09 15:09:00 -0600

    Simplifying and adding the insert function

--- Solar/Caster/Utility/Delta.hs
+++ Solar/Caster/Utility/Delta.hs
@@ -18,29 +18,18 @@ type QueueZipper a = (Queue a, Queue a)
 makeZipper :: Queue a -> QueueZipper a
 makeZipper a = ([], a)
 
-collectZipper :: Integer -> QueueZipper a -> QueueZipper a
-collectZipper _ (o, []) = (o, [])
-collectZipper i (o, x:xs)
-    | r == 0 = collectZipper i (x:o, xs)
-    | r <= i = collectZipper (i - r) (x:o, xs)
-    | r > i = (o, x':xs)
-    | otherwise = (o, x:xs)
-    where
-        r = remaining x
-        x' = x {remaining = r - i}
-
 sumQueue :: Queue a -> Integer
 sumQueue as = sum $ map remaining as
 
-splitWhen' :: (a -> b -> (Bool, b)) -> b -> [a] -> Int -> (Int, b)
-splitWhen' _ b [] a = (a, b)
-splitWhen' f a (v:vs) r
-    | tf = splitWhen' f a' vs (r+1)
+splitWhen :: (a -> b -> (Bool, b)) -> b -> [a] -> Int -> (Int, b)
+splitWhen _ b [] a = (a, b)
+splitWhen f a (v:vs) r
+    | tf = splitWhen f a' vs (r+1)
     | otherwise = (r, a')
     where (tf, a') = f v a
 
-splitZipper :: Queue a -> DeltaContainer a -> QueueZipper a
-splitZipper qs c = (before, c':after)
+insertZipper :: Queue a -> DeltaContainer a -> QueueZipper a
+insertZipper qs c = (before, c':after)
     where
         rc = remaining c
         (before, after) = zipTo qs rc
@@ -53,16 +42,23 @@ zipperSplitter v a
     | otherwise = (False, a)
     where v' = remaining v
 
-
-mergeZipper :: QueueZipper a -> Queue a
-mergeZipper (as, bs) = as ++ bs
+unsplitZipper :: QueueZipper a -> Queue a
+unsplitZipper (as, bs) = as ++ bs
 
 zipTo :: Queue a -> Integer -> QueueZipper a
-zipTo qs rc = (before', after')
+zipTo qs rc = (before, after')
     where
-        (i, amt) = splitWhen' zipperSplitter rc qs 0
+        (i, amt) = splitWhen zipperSplitter rc qs 0
         (before, after) = splitAt i qs
-        before' = map (\x -> x {remaining = 0}) before
         after' = case after of
             [] -> []
             (x:xs) -> let x' = x {remaining = (remaining x) - amt} in x':xs 
+
+zipOver :: Queue a -> Integer -> QueueZipper a
+zipOver qs rc = (before', after)
+    where
+        (before, after) = zipTo qs rc
+        before' = map (\x -> x {remaining = 0}) before
+
+insert :: DeltaContainer a -> Queue a -> Queue a
+insert c q = unsplitZipper $ insertZipper q c
