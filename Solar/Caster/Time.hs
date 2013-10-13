bff1c053fe277dae3689e9a0633b4c2880fdceea
Levi Schuck <levi.schuck@gmail.com>
2013-10-12 22:12:35 -0600

    Now things seem to be working under the new mechanism.

--- Solar/Caster/Time.hs
+++ Solar/Caster/Time.hs
@@ -1,6 +1,7 @@
 module Solar.Caster.Time where
 
 import Control.Concurrent.STM
+import Control.Concurrent
 import Solar.Utility.Delta
 import Data.Time
 import Control.Monad
@@ -20,12 +21,12 @@ instance Eq Ticker where
 instance Ord Ticker where
     compare a b = compare (toTick a) (toTick b)
 
-type TickerD = Queue Ticker
+type TickerD = Queue Ticker NominalDiffTime
+type DCTicker = DeltaContainer Ticker NominalDiffTime
 
 data TickingClock = TickingClock
     { deltaTicker :: TVar TickerD
     , lastTick    :: UTCTime
-    , tickInterval:: NominalDiffTime
     }
 
 
@@ -35,40 +36,48 @@ incClock t = do
     atomically $ do
         c <- readTVar t
         let diff = diffUTCTime time (lastTick c)
-            int = tickInterval c
-            amt = floor $ diff / int
-        replicateM_ amt $ incOnce (time, int) $ deltaTicker c
-        when (amt > 0) $ writeTVar t $ c {lastTick = time}
+        incClock' time diff t
+        writeTVar t $ c {lastTick = time}
 
-
-incOnce :: (UTCTime, NominalDiffTime) -> TVar TickerD -> STM ()
-incOnce (time, int) td = do
-    d <- readTVar td
-    let (ticked, later) = zipTo d 1
-    forM_ ticked $ applyTick time
-    newTicks <- mapM up ticked
-    let updatedTicks = zipWith f ticked newTicks
-        newList = foldr insert later updatedTicks
-    writeTVar td newList
+incClock' :: UTCTime -> NominalDiffTime -> TVar TickingClock -> STM ()
+incClock' time dr t = do
+    c <- readTVar t
+    dt <- readTVar $ deltaTicker c
+    unless (null dt) $ do
+        -- We definitely have something to deduct from
+        let (ticked, later) = zipTo dt dr
+        forM_ ticked $ applyTick time
+        newTicks <- mapM up ticked
+        let updatedTicks = zipWith f ticked newTicks
+            newList = foldr insert later updatedTicks
+        writeTVar (deltaTicker c) newList
     where
-        up = upClock (time, int)
+        up = upClock time
         f v r = v {remaining = r}
 
-upClock :: (UTCTime, NominalDiffTime) -> DeltaContainer Ticker -> STM (Integer)
-upClock (time, int) t' = do
+
+
+getNextLength :: TickerD -> NominalDiffTime
+getNextLength d = case d of
+    (x:_) -> toTick.content $ x
+    _ -> 86400 -- 1 day
+
+
+upClock :: UTCTime -> DCTicker -> STM (NominalDiffTime)
+upClock time t' = do
     let td = content t'
         tt = tick td
     t <- readTVar tt
-    upClock' (time, int) t td 
+    upClock' time t td 
     
-upClock' :: (UTCTime, NominalDiffTime) -> Tick -> Ticker -> STM (Integer)
-upClock' (time, int) t tv
-    | (tickLength t) /= (toTick tv) = upClock' (time, int) (t {tickLength = toTick tv}) tv
+upClock' :: UTCTime -> Tick -> Ticker -> STM (NominalDiffTime)
+upClock' time t tv
+    | (tickLength t) /= (toTick tv) = upClock' time (t {tickLength = toTick tv}) tv
     -- ↑ Sanity check so people can't change stuff ad hoc
-    | not diffB = return ratioi
+    | not diffB = return r
     | otherwise = do
         writeTVar (tick tv) $ t {ticks = tticks+1, tickedLast = time}
-        return ratioi
+        return r
     where
         lastT = tickedLast t
         ti = tickLength t
@@ -76,13 +85,12 @@ upClock' (time, int) t tv
         tid = ti - diff
         diffB = tid <= 0
         -- ↑ When this is a tick.
-        ratio = if diffB then ti / int else tid / int
-        ratioi = ceiling ratio
+        r = if diffB then ti else tid
         tticks = ticks t
 
 
 
-applyTick :: UTCTime -> DeltaContainer Ticker -> STM ()
+applyTick :: UTCTime -> DCTicker -> STM ()
 applyTick time tk' = do
     let tk = content tk'
     k <- readTVar $ tick tk 
@@ -98,9 +106,7 @@ getClockTicker tc diff = do
         (x:_) -> return $ tick.content $ x
         _ -> do
             tv <- newTVar $ Tick 0 (lastTick c) diff
-            let int = tickInterval c
-                amt = ceiling $ diff / int
-                dc = DeltaContainer (Ticker tv diff) amt
+            let dc = DeltaContainer (Ticker tv diff) diff
                 newq = insert dc td
             writeTVar (deltaTicker c) newq
             return tv
@@ -109,24 +115,46 @@ getClockTicker tc diff = do
 
 
 
--- mkDemoTicker time i' = do
---     let i = fromInteger i'
---         t = Tick 0 time i
---     tv <- newTVar t
---     return $ DeltaContainer (Ticker tv i) i'
--- mkDemo = do
---     time <- getCurrentTime
---     atomically $ do
---         ts <- mapM (mkDemoTicker time) [1..10]
---         qts <- newTVar ts
---         tsv <- newTVar $ TickingClock qts time 1
---         return tsv
--- 
--- showDemo tc' = do
---     putStrLn "Current List:"
---     ts <- atomically $ do
---         tc <- readTVar tc'
---         t <- readTVar $ deltaTicker tc
---         forM t $ \tk -> readTVar $ tick.content $ tk
---     forM_ ts $ \t -> do
---         putStrLn $ show t
+mkDemoTicker time i' = do
+    let i = fromInteger i'
+        t = Tick 0 time i
+    tv <- newTVar t
+    return $ DeltaContainer (Ticker tv i) i
+mkDemo = do
+    time <- getCurrentTime
+    atomically $ do
+        ts <- mapM (mkDemoTicker time) [1..10]
+        qts <- newTVar $ foldr insert [] ts
+        tsv <- newTVar $ TickingClock qts time
+        return tsv
+
+showDemo tc' = do
+    putStrLn "Current List:"
+    ts <- atomically $ do
+        tc <- readTVar tc'
+        t <- readTVar $ deltaTicker tc
+        forM t $ \tk -> do
+            v <- readTVar $ tick.content $ tk
+            return (v, remaining tk)
+    forM_ ts $ \t -> do
+        putStrLn $ show t
+
+doDemo x = do
+    d <- mkDemo
+    showDemo d
+    time <- getCurrentTime
+    let endtime = addUTCTime (fromInteger x) time
+    stepUntil d time 1.0000001 endtime
+doDemo' x = do
+    d <- mkDemo
+    showDemo d
+    replicateM_ x $ do
+        threadDelay 1000000
+        incClock d
+        showDemo d
+
+stepUntil d time diff endtime = do
+    atomically $ incClock' ntime diff d
+    showDemo d
+    when (ntime < endtime) $ stepUntil d ntime diff endtime
+    where ntime = addUTCTime diff time
